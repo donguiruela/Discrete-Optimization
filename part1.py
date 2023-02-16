0cb39c0366974103289ea0d00612454258aa53b8
Don Guiruela <guirueladon@gmail.com>
2023-02-16 16:09:32 +0800

    Update part1.py

--- part1.py
+++ part1.py
@@ -12,7 +12,7 @@
 
         # iterate each item        
 
-        for item in sample:
+        for item in sample :
             if weight + item.weight <= capacity :
                 taken[item.index] = 1
                 value += item.value
@@ -32,13 +32,13 @@
         # determine the index of the max value to match its corresponding taken value
 
         for i in range(0, len(value_arr)) :
-            if value_arr[i] > ref:
+            if value_arr[i] > ref :
                 max = i
                 ref = value_arr[i]            
                
         # output                
 
-        if len(sample) == 0:            
+        if len(sample) == 0 :            
             output_data = str(value_arr[max]) + ' ' + str(1) + '\n'
             output_data += ' '.join(map(str, taken_arr[max]))
             return output_data
